29c2773962c00c4e210267601065cacfb778bbd0
Ahmed Abdirizaak <mernstack.js@gmail.com>
2024-09-18 20:23:39 +0200

    waxa aan ku daray react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 import React from "react";
+import { Link, Route, Routes } from "react-router-dom";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <nav>
+        <Link to={"/"}>Home</Link>
+        <Link to={"/login"}>login</Link>
+        <Link to={"/profile"}>profile</Link>
+        <Link to={"/content"}>content</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<div>Home Page</div>} />
+        <Route path="/profile" element={<div>Home profile</div>} />
+        <Route path="/login" element={<div>Home Login</div>} />
+        <Route path="/content" element={<div>Home Content</div>} />
+      </Routes>
+    </div>
+  );
 }
